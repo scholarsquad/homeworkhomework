81e0fac6c0f9fffd0e1b9cfa0001285f3fdfa903
scholarsquad <scholarsquadservice@gmail.com>
2025-05-22 17:22:31 -0700

    Update and rename script.js to index.html

--- script.js
+++
@@ -1,9 +0,0 @@
-function doGet() {
-  return HtmlService.createHtmlOutput(`
-    <html>
-      <body style="margin:0;padding:0;">
-        <script src="https://minnit.chat/js/embed.js?c=1740011833" defer></script><span style="display: none;" class="minnit-chat-sembed" data-chatname="https://organizations.minnit.chat/999193744516631/ScholarChat?embed" data-style="width:100%; height:5000px; max-height:100vh;">Chat</span><p class="powered-by-minnit"><a href="https://minnit.chat" target="_blank"></a></p>
-      </body>
-    </html>
-  `)
-}
